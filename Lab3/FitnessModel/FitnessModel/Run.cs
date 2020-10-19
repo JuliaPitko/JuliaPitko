214ecf29bbe16564fffb3ef8b97fba79d5e90229
JuliaPitko <julia14032007@mail.ru>
2020-10-19 21:36:05 +0700

    New

---
+++ Lab3/FitnessModel/FitnessModel/Run.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessModel
+{
+    /// <summary>
+    /// Класс для описания бега
+    /// </summary>
+    public class Run : ActionsBase
+    {
+        /// <summary>
+        /// поле интенсивности
+        /// </summary>
+        private double _intensive;
+
+        /// <summary>
+        /// интенсивность бега
+        /// </summary>
+        public double Intensive 
+        {
+            get
+            {
+                return _intensive;
+            }
+            set
+            {
+                _intensive = ValueChecking(value);
+            }
+        }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="intensive">интенсивность</param>
+        /// <param name="weightPerson">вес человека</param>
+        /// <param name="time">время</param>
+        /// <param name="distanceOrWeight">дистанция</param>
+        public Run(double intensive, double weightPerson,
+                    double time, double distanceOrWeight)
+        {
+            Intensive = intensive;
+            WeightPerson = weightPerson;
+            Time = time;
+            DistanceOrWeight = distanceOrWeight;
+        }
+
+        /// <summary>
+        /// Пустой конструктор
+        /// </summary>
+        public Run() { }
+
+       /// <summary>
+       /// Конструктор через общие для всех параметры
+       /// </summary>
+       /// <param name="weightPerson"></param>
+       /// <param name="time"></param>
+        public Run (double weightPerson, double time)
+        {
+            WeightPerson = weightPerson;
+            Time = time;
+        }
+
+        /// <summary>
+        /// Расчёт калорий при беге
+        /// </summary>
+        /// <returns>количество калорий</returns>
+        public override double KkalCalculation
+        {
+            get 
+            {
+                return WeightPerson * Time * (DistanceOrWeight / Intensive);
+            }
+            
+        }
+    }
+}
---
+++ Lab3/FitnessModel/TestingApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessModel;
+
+namespace TestingApp
+{
+    /// <summary>
+    /// Тестирующий класс
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// точка вхождения в программу
+        /// </summary>
+        /// <param name="args">аргументы</param>
+        static void Main(string[] args)
+        {
+            while (true )
+            {
+                Console.Write("Выберите тип движения:\n" +
+                $"1 - бег\n" +
+                $"2 - плавание\n" +
+                $"3 - жим штанги\n" +
+                $"4 - чтобы выйти\n");
+
+                var action = ChooseActionsType();
+                Console.WriteLine("Ответ: " + action.KkalCalculation());
+            }
+        }
+
+        /// <summary>
+        /// Выбор вида спорта
+        /// </summary>
+        /// <returns></returns>
+        private static ActionsBase ChooseActionsType()
+        {
+            while (true)
+            {
+                try
+                {
+                    var number = int.Parse(Console.ReadLine());
+                    switch (number)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Введите параметры для бега: ");
+                                return ConsoleLoader.EnterRunParameters();
+                            }
+
+                        case 2:
+                            {
+                                Console.WriteLine("Введите параметры для плавания: ");
+                                return ConsoleLoader.EnterSwimmingParameters();
+                            }
+
+                        case 3:
+                            {
+                                Console.WriteLine("Введите параметры для жима штанги: ");
+                                return ConsoleLoader.EnterBenchPressParameters();
+                            }
+
+                        case 4:
+                            {
+                                Environment.Exit(0);
+                                break;
+                            }
+
+                        default:
+                            {
+                                throw new ArgumentException("Такого номера нет! Введите повторно");
+                            }
+                    }
+
+                }
+
+                catch (Exception exception)
+                {
+                    if (exception is ArgumentException
+                        || exception is FormatException)
+                    {
+                        Console.WriteLine($"\n{exception.Message}\n");
+                    }
+                }
+            }
+        }
+    }
+}
